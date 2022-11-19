40d912fb1ed40b137ac84e29656ff3844cd38dd3
Vladimir Stepankin <vladimirstepankin@mail.ru>
2022-11-19 15:50:57 +0300

    Добавил второй вариант первой задачи

---
+++ Exs1_2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число
+и проверяет, является ли оно палиндромом.*/
+
+int number = ReadInt("Введите пятизначное число: ");
+
+int temp = number;
+int revers = 0;
+int i = 0;
+if (number > 9999 && number < 1000000)
+{
+    while (number > 0)
+    {
+        i = number % 10;
+        revers = revers * 10 + i;
+        number = number / 10;
+    }
+    if (temp == revers)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else Console.Write("Введено неверное число");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
